f8216a3c658639d590faa5dfb2e114cd9b6c936d
Md Main Uddin <2021md.main@gmail.com>
2022-10-11 14:47:39 +0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const PORT = 3006  ;
+const app =  express()
+
+const hello = "test"
+
+
+app.get('/test',(req,res)=>{
+    res.json({status:true , message:"server respond successfully"})
+})
+app.listen(PORT,()=>console.log("server is running on 3006"))
